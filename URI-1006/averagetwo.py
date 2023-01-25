afd7dc00b18f91f03d2b9e91ed205f817e159479
Matheus Silva <msantossilva005@gmail.com>
2023-01-25 17:51:48 -0300

    URI-1006

---
+++ URI-1006/averagetwo.py
@@ -0,0 +1,7 @@
+# Desafio URI - BeeCrowd 1005.
+# "Average 2"
+A = float(input())
+B = float(input())
+C = float(input())
+M = ((A * 2.0) + (B * 3.0) + (C * 5.0)) / (2.0 + 3.0 + 5.0)
+print("MEDIA = %.1f" %M)
