73fe5d153f7c21dc5def5f6085659114f81165b3
Matheus Silva <msantossilva005@gmail.com>
2023-01-29 23:31:32 -0300

    URI-1009

---
+++ URI-1009/salarybonus.py
@@ -0,0 +1,7 @@
+# Desafio URI - BeeCrowd 1009.
+# "Salary With Bonus"
+Nome = input()
+Salary = float(input())
+Bonus = float(input())
+total = (Bonus * 0.15) + Salary
+print("TOTAL = R$ %.2f" %total)
